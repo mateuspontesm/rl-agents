b5a539f22a34786a6481455a7ff05aa96ad99b5a
Mateus Mota <mateuspontesm@hotmail.com>
2020-05-26 19:59:17 -0300

    Added init and made some styling changes.

--- tests/test_rl_agents.py
+++ tests/test_rl_agents.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """Tests for `rl_agents` package."""
 
 import pytest
@@ -22,4 +21,4 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    assert example.some_function(1,2) == 1 + 2
+    assert example.some_function(1, 2) == 1 + 2
