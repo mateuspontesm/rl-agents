3ebcc44de63a704c18f61eae96cbd3d9054fe305
Mateus Mota <mateuspontesm@hotmail.com>
2020-05-27 19:21:50 -0300

    added ignore comment to flake8

--- src/rl_agents/agents/__init__.py
+++ src/rl_agents/agents/__init__.py
@@ -3,4 +3,4 @@ The :mod:`rl_agents.agents` module includes the RL agents classes and utilities.
 It includes the MAB variants, tabular methods and Deep RL.
 """
 
-import rl_agents.agents.mab
+import rl_agents.agents.mab  # noqa: F401
