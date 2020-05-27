fdf50473425145c658bc67bdc468bb514e6b034e
Mateus Mota <mateuspontesm@hotmail.com>
2020-05-26 23:40:08 -0300

    Styling Changes to please isort

--- src/rl_agents/agents/mab/__init__.py
+++ src/rl_agents/agents/mab/__init__.py
@@ -1,7 +1,5 @@
-from rl_agents.agents.mab.egreedy import (  # noqa: F401
-    DecayEpsilon,  # noqa: F401
-    EpsilonGreedy,  # noqa: F401
-)  # noqa: F401
+from rl_agents.agents.mab.egreedy import DecayEpsilon  # noqa: F401
+from rl_agents.agents.mab.egreedy import EpsilonGreedy  # noqa: F401
 from rl_agents.agents.mab.pursuit import Pursuit  # noqa: F401
 from rl_agents.agents.mab.softmax import Softmax  # noqa: F401
 from rl_agents.agents.mab.ucbs import UCB, UCB1, UCB2  # noqa: F401
