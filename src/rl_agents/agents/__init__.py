1d4ec5f3497afd05540293bf85afede8f5034e08
Mateus Mota <mateuspontesm@hotmail.com>
2020-05-27 19:26:19 -0300

    Flake8 error.

--- src/rl_agents/agents/__init__.py
+++ src/rl_agents/agents/__init__.py
@@ -1,6 +1,7 @@
 """
-The :mod:`rl_agents.agents` module includes the RL agents classes and utilities.
-It includes the MAB variants, tabular methods and Deep RL.
+The :mod:`rl_agents.agents` module includes the RL agents
+classes and utilities. It includes the MAB variants,
+tabular methods and Deep RL.
 """
 
 import rl_agents.agents.mab  # noqa: F401
