66ad09f0a23e21775c14e887ed05343eede30ca4
Mateus Mota <mateuspontesm@hotmail.com>
2020-05-26 21:32:10 -0300

    Added testing script for testing mab env

---
+++ tests/test_envs.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from rl_agents.envs import BanditKArmedGaussianEnv
+
+
+def test_k_armed_env():
+    env = BanditKArmedGaussianEnv()
+    _ = env.reset()
+    for _ in range(10):
+        action = env.action_space.sample()
+        state, reward, done, info = env.step(action)
