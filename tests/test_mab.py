accab1ee3eb349252b5d6c35163f7f3b7a49b6f5
Mateus Mota <mateuspontesm@hotmail.com>
2020-05-27 18:31:28 -0300

    Put all tests into a single function.

--- tests/test_mab.py
+++ tests/test_mab.py
@@ -1,83 +1,32 @@
 import numpy as np
 
-from rl_agents.envs import BanditKArmedGaussianEnv
-from rl_agents.runners import simple_mab_runner
 from rl_agents.agents.mab import (
-    EpsilonGreedy,
-    DecayEpsilon,
-    Softmax,
-    Pursuit,
     UCB,
     UCB1,
     UCB2,
+    DecayEpsilon,
+    EpsilonGreedy,
+    Pursuit,
+    Softmax,
 )
+from rl_agents.envs import BanditKArmedGaussianEnv
+from rl_agents.runners import simple_mab_runner
 
 
-def test_egreedy():
+def test_all_mabs():
     env = BanditKArmedGaussianEnv()
-    agent = EpsilonGreedy(env.action_space.n, 0.1)
-    n_trials = 30
-    rewards, regrets, optimals = simple_mab_runner(env, agent, n_trials)
-    assert len(rewards) == n_trials
-    assert len(regrets) == n_trials
-    assert len(optimals) == n_trials
-
-
-def test_decay_greedy():
-    env = BanditKArmedGaussianEnv()
-    agent = DecayEpsilon(env.action_space.n, 0.5, 0.99)
-    n_trials = 30
-    rewards, regrets, optimals = simple_mab_runner(env, agent, n_trials)
-    assert len(rewards) == n_trials
-    assert len(regrets) == n_trials
-    assert len(optimals) == n_trials
-
-
-def test_ucb():
-    env = BanditKArmedGaussianEnv()
-    agent = UCB(env.action_space.n, 0.005)
-    n_trials = 30
-    rewards, regrets, optimals = simple_mab_runner(env, agent, n_trials)
-    assert len(rewards) == n_trials
-    assert len(regrets) == n_trials
-    assert len(optimals) == n_trials
-
-
-def test_ucb1():
-    env = BanditKArmedGaussianEnv()
-    agent = UCB1(env.action_space.n)
-    n_trials = 30
-    rewards, regrets, optimals = simple_mab_runner(env, agent, n_trials)
-    assert len(rewards) == n_trials
-    assert len(regrets) == n_trials
-    assert len(optimals) == n_trials
-
-
-def test_ucb2():
-    env = BanditKArmedGaussianEnv()
-    agent = UCB2(env.action_space.n, 0.01)
-    n_trials = 30
-    rewards, regrets, optimals = simple_mab_runner(env, agent, n_trials)
-    assert len(rewards) == n_trials
-    assert len(regrets) == n_trials
-    assert len(optimals) == n_trials
-
-
-def test_softmax():
-    env = BanditKArmedGaussianEnv()
-    agent = Softmax(env.action_space.n, 0.02)
-    n_trials = 30
-    rewards, regrets, optimals = simple_mab_runner(env, agent, n_trials)
-    assert len(rewards) == n_trials
-    assert len(regrets) == n_trials
-    assert len(optimals) == n_trials
-
-
-def test_pursuit():
-    env = BanditKArmedGaussianEnv()
-    agent = Pursuit(env.action_space.n, 0.1)
-    n_trials = 30
-    rewards, regrets, optimals = simple_mab_runner(env, agent, n_trials)
-    assert len(rewards) == n_trials
-    assert len(regrets) == n_trials
-    assert len(optimals) == n_trials
+    agents = [
+        EpsilonGreedy(env.action_space.n, 0.1),
+        DecayEpsilon(env.action_space.n, 0.5, 0.99),
+        UCB(env.action_space.n, 0.005),
+        UCB1(env.action_space.n),
+        UCB2(env.action_space.n, 0.01),
+        Softmax(env.action_space.n, 0.02),
+        Pursuit(env.action_space.n, 0.1),
+    ]
+    for agent in agents:
+        n_trials = 30
+        rewards, regrets, optimals = simple_mab_runner(env, agent, n_trials)
+        assert len(rewards) == n_trials
+        assert len(regrets) == n_trials
+        assert len(optimals) == n_trials
