cdff14b515111d6a55b09719621a8760335f340d
Mateus Mota <mateuspontesm@hotmail.com>
2020-06-01 23:17:00 -0300

    Minor updates to please flake8

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 
 from rl_agents.envs import BanditKArmedGaussianEnv
 
--- tests/test_mab.py
+++ tests/test_mab.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 
 from rl_agents.agents.mab import (
     UCB,
